5072b0de31c575de5e07e42b42e1e0ccc93e0a36
Chrystyan Byrnes Santos de Olinda <chrystyan96@hotmail.com>
2017-09-27 17:29:13 -0300

    atualizando repositório

--- site2.0/pages/js/geralSite.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function(){
-  $('#novoCadastro #telefone').mask('(00) 0000-00000', {placeholder: "(__) ____-_____", selectOnFocus: true, clearIfNotMatch: true});
-  $('#editarCad #telefone').mask('(00) 0000-00000', {placeholder: "(__) ____-_____", selectOnFocus: true, clearIfNotMatch: true});
-});
