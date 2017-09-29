ec1e4278c0b9d696ab9f461fdff56c6d6a47ab45
Chrystyan Byrnes Santos de Olinda <chrystyan96@hotmail.com>
2017-09-29 13:31:09 -0300

    atualizando pasta

--- site2.0/pages/js/adm/detalhesPedido.js
+++
@@ -1,9 +0,0 @@
-$('.detalhes').click(function () {
-    var id = $(this).attr('data-id');
-    $.ajax({url: "../regras/adm/regrasDetalhesPedidos.php?id=" + id,
-        cache: false,
-        success: function (result) {
-            $(".opcoes").html(result);
-        }
-    });
-});
--- site2.0/pages/js/cliente/editarCadastro.js
+++
@@ -1,9 +0,0 @@
-$('.editCadastro').click(function () {
-    var id = $('.painelCliente .cliPainel .editCadastro').attr('data-id');
-    $.ajax({url: "../regras/cliente/editarCadastro.php?id=" + id,
-        cache: false,
-        success: function (result) {
-            $(".editarCadastro").html(result);
-        }
-    });
-});
